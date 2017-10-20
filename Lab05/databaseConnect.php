acef91be5a734c8e5c265a49acfb7691ff95804e
Matthew Connolly <matt.connolly1225@gmail.com>
2017-10-20 06:58:35 +0000

    heroku

--- Lab05/databaseConnect.php
+++ Lab05/databaseConnect.php
@@ -9,13 +9,13 @@ function getDatabaseConnection(){
     $password = "cst336";
     
     //using different database variables in Heroku
-    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    //     $host = $url["host"];
-    //     $dbname = substr($url["path"], 1);
-    //     $username = $url["user"];
-    //     $password = $url["pass"];
-    // } 
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
     
     //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
