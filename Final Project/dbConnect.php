84c10d20b9a85d931da25a319e39378965b37722
Matthew Connolly <matt.connolly1225@gmail.com>
2017-12-10 09:12:54 +0000

    heroku stuff

--- Final Project/dbConnect.php
+++ Final Project/dbConnect.php
@@ -3,19 +3,19 @@
 
 function getDatabaseConnection(){
     
-    $dbHost = 'localhost';//cloud 9
-    $dbPort = '3306';
-    $dbName = 'hlm';
-    $username = 'connmatt';
-    $password = 'cst336';
+    // $dbHost = 'localhost';//cloud 9
+    // $dbPort = '3306';
+    // $dbName = 'hlm';
+    // $username = 'connmatt';
+    // $password = 'cst336';
     //using different database variables in Heroku
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+   // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $dbHost = getenv('DATABASE_HOST');
         $dbPort = getenv('DATABASE_HOST');
         $dbName = getenv('DATABASE_NAME');
         $username = getenv('USERNAME');
         $password = getenv('PASSWORD');
-    } 
+   //} 
     
     //creates db connection
     $dbConn = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName", $username, $password);
